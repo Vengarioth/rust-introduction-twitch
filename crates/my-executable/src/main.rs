2b0695e6382756ec871c56c552125bd1a403bb22
Andreas Fischer <opensource@deviru.de>
2021-09-17 13:34:58 +0200

    added Point type

--- crates/my-executable/src/main.rs
+++ crates/my-executable/src/main.rs
@@ -1,5 +1,31 @@
 use my_library::*;
 
+#[derive(Debug, Clone, Copy)]
+struct Point {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Point {
+    fn new(x: f32, y: f32) -> Self {
+        Self {x, y}
+    }
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let a = Point::new(0.0, 1.0);
+    let b = Point::new(1.0, 0.0);
+
+    let c = a.add(b);
+
+    dbg!(a, b, c);
 }
